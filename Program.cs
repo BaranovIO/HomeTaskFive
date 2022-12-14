0bf0af8ef7ba0cc3fb987827c771d65c15ff80b1
Igor <eltery@mail.ru>
2022-12-14 19:24:57 +0300

    Sum of elements on odd indexes done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿
+﻿void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+      Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+/*
 int[] CreateRandomArray(int size)
 {
     int[] array = new int[size];
@@ -8,15 +17,6 @@ int[] CreateRandomArray(int size)
         array[i] = new Random().Next(100, 1000);  
     
     return array;
-}
-
-void ShowArray(int[] array)
-{
-    for(int i = 0; i < array.Length; i++)
-    {
-      Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
 }
 
 int EvenNumberCount(int[] array)
@@ -35,8 +35,47 @@ int length = Convert.ToInt32(Console.ReadLine());
 
 int[] myArray = CreateRandomArray(length);
 ShowArray(myArray);
+
 int result = EvenNumberCount(myArray);
 Console.WriteLine("Количество чётных элементов массива: " + result);
+*/
+
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)   
+
+        array[i] = new Random().Next(minValue, maxValue + 1);  
+    
+    return array;
+}
+
+int SumOfElementsOnOddIndex(int[] array)
+{
+    int sum = 0;
+    for(int i = 1; i < array.Length; i+=2)
+       
+        sum += array[i];
+    
+    return sum;
+}
+
+Console.Write("Input a number of elements: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(length, min, max);
+ShowArray(myArray);
+
+int result = SumOfElementsOnOddIndex(myArray);
+Console.WriteLine("The sum of elements on odd indexes is " + result);
+
+
 
 
 
